ad55da8ac3dd1334cd1c37be55a1a22a202c1188
Waylon Peng <dragxtcon@users.noreply.github.com>
2017-09-14 00:48:28 -0700

    Added Sublime Text project files and name.cpp

---
+++ name.cpp
@@ -0,0 +1,11 @@
+// Waylon Peng, Period 1
+// Sep092017
+// Lab 1: Name Box
+// Writes my name inside a box
+
+#include <iostream>
+
+int main() {
+    std::cout << "----------\n| Waylon |\n----------" << std::endl; //Outputs my name inside a box
+    return 0;
+}
